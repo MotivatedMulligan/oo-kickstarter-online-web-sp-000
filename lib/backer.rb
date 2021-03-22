20972a043a82becfa1868131843bfc7757f032d6
Sean Mulligan <sean.paul.mulligan@gmail.com>
2021-03-22 23:03:55 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,15 +1,15 @@
 
 class Backer
-  attr_reader :backed_projects, :name
+  attr_reader :name, :backed_projects 
   def initialize(name)
     @name = name
-  @backed_projects = []
+    @backed_projects = []
     # takes a name on initialization, accessible through an attribute reader
     # initializes with a @backed_projects attribute, an empty array
   end
 
-  def back_project(backed_projects)
-    @backed_projects << backed_projects
+  def back_project(project)
+    @backed_projects << project
     project.backers << self
     # accepts a Project as an argument and stores it in a backed_projects array
     # also adds the backer to the project's backers array
